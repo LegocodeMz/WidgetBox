d32e4b4b592470bef9929cc0c34a4925b487109b
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-08-30 21:26:12 +0200

    New: Adding the first snackbar widget

---
+++ lib/widgets/snacks/default_snack.dart
@@ -0,0 +1,47 @@
+import 'package:aprova/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class DefaultSnackBar extends StatelessWidget {
+
+  void defaultSnackBar(BuildContext context, String message) {
+     ScaffoldMessenger.of(context)
+        .showSnackBar(
+        SnackBar(
+          action: SnackBarAction(
+            label: 'OK',
+            onPressed: ScaffoldMessenger.of(context).hideCurrentSnackBar,
+            textColor: BLUE,
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.warning,
+                color: WHITE,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Text(
+                  message,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: WHITE,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0),
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: MOZTOP_RED,
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
