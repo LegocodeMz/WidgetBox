db6d2ba69c852690bb01417b839470a9c040ab8e
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-08-30 21:25:06 +0200

    New: Adding a snippet to handle basic colors inside the app

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const Color BLUE = Color(0xFF3F51B5);
+const Color ACCENT = Color(0xFFE91E63);
+const Color PRIMARY = Color(0xFF2196F3);
+const Color PRIMARY_DARK = Color(0xFF1976D2);
+const Color PRIMARY_LIGHT = Color(0xFFBBDEFB);
+const Color SECONDARY = Color(0xFF9C27B0);
+const Color SECONDARY_DARK = Color(0xFF7B1FA2);
+const Color SECONDARY_LIGHT = Color(0xFFE1BEE7);
+const Color WHITE = Color(0xFFFFFFFF);
+const Color BLACK = Color(0xFF000000);
+const Color GREY = Color(0xFF9E9E9E);
+const Color GREY_LIGHT = Color(0xFFE0E0E0);
+const Color GREY_DARK = Color(0xFF616161);
+const Color RED = Color(0xFFF44336);
+const Color RED_LIGHT = Color(0xFFFFCDD2);
+const Color RED_DARK = Color(0xFFD32F2F);
+const Color GREEN = Color(0xFF4CAF50);
+const Color GREEN_LIGHT = Color(0xFFDCEDC8);
+const Color GREEN_DARK = Color(0xFF388E3C);
+const Color YELLOW = Color(0xFFFFEB3B);
+const Color YELLOW_LIGHT = Color(0xFFFFFF9C);
+const Color YELLOW_DARK = Color(0xFFFBC02D);
+const Color ORANGE = Color(0xFFFF9800);
+const Color ORANGE_LIGHT = Color(0xFFFFECB3);
+const Color ORANGE_DARK = Color(0xFFF57C00);
+const Color PINK = Color(0xFFE91E63);
+const Color PINK_LIGHT = Color(0xFFF8BBD0);
+const Color PINK_DARK = Color(0xFFC2185B);
+const Color PURPLE = Color(0xFF9C27B0);
+const Color PURPLE_LIGHT = Color(0xFFE1BEE7);
+const Color PURPLE_DARK = Color(0xFF7B1FA2);
+const Color TEAL = Color(0xFF009688);
+const Color TEAL_LIGHT = Color(0xFFB2DFDB);
+const Color TEAL_DARK = Color(0xFF00796B);
+const Color BROWN = Color(0xFF795548);
+const Color BROWN_LIGHT = Color(0xFFD7CCC8);
+const Color BROWN_DARK = Color(0xFF5D4037);
+const Color BLUE_GREY = Color(0xFF607D8B);
+const Color BLUE_GREY_LIGHT = Color(0xFFCFD8DC);
+const Color BLUE_GREY_DARK = Color(0xFF455A64);
+
+// Language: dart
+// Path: lib\utils\constants\colors.dart
+// Use the constants above to define your own colors
+// Made with ❤ in Mozambique by @nakatsinho
