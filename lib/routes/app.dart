f6e0ab0eda1bf84782f528ccbcb0a509ffed7b42
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-08-30 21:23:57 +0200

    Update: Adding route file to handle links inside the app

---
+++ lib/routes/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  Main.routeNamed: (context) => new Main(),
+};
+
+// Language: dart
+// Path: lib\main.dart
+// Use this snippet to define your own routes
+// Made with ❤ in Mozambique by @nakatsinho
