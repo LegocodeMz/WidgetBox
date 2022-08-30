9a4fdae0da579a598846c74391fed3e27d7455a6
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-08-30 21:25:32 +0200

    New: Adding the first button widget

---
+++ lib/widgets/buttons/default_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton(
+      {Key? key, this.text, this.press, this.color, this.backColor})
+      : super(key: key);
+  final Color? color;
+  final Color? backColor;
+  final String? text;
+  final Function()? pressAction; //TODO:: It possible to use VoidCallBack? instead of Function
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 56.0,
+      child: ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(35))),
+            backgroundColor: MaterialStateProperty.all(backColor)),
+        onPressed: pressAction,
+        child: Text(
+          text!,
+          style: TextStyle(
+            fontSize: 18,
+            color: color,
+          ),
+        ),
+      ),
+    );
+  }
+}
