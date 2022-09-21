7076a786c1da2e5ea6ee0ee86165cb0319647a08
Kelton M. Cumbe <nakatsinho@gmail.com>
2022-09-21 10:22:24 +0200

    Update: Adding new routes and body scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:widgetbox/routes/app.dart';
-import 'package:widgetbox/utils/constants/colors.dart';
+import 'package:provider/provider.dart';
+import 'package:widgetbox/utils/themes/theme_factory.dart';
+import 'package:widgetbox/utils/themes/theme_provider.dart';
 import 'package:widgetbox/views/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeProvider themeChangeProvider = new ThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.theme =
+        await themeChangeProvider.themePreference.getTheme();
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WidgetBox',
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          primaryColor: BLUE,
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
-              .copyWith(secondary: ACCENT)),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: MainPage.routeNamed,
-      routes: routes,
-      debugShowCheckedModeBanner: false,
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
+      },
+      child: Consumer<ThemeProvider>(
+          builder: (BuildContext context, value, Widget? child) {
+        return MaterialApp(
+          title: 'WidgetBox',
+          theme: ThemeFactory.getTheme(themeChangeProvider.themeMode),
+          initialRoute: MainPage.routeNamed,
+          routes: routes,
+          debugShowCheckedModeBanner: false,
+        );
+      }),
     );
   }
 }
